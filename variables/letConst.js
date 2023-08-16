0ddfef93d9bb19e1b9429d81c03c4322dfe7357a
PraveenKola11 <107261366+PraveenKola11@users.noreply.github.com>
2023-08-16 17:42:12 +0530

    VF commit

---
+++ variables/letConst.js
@@ -0,0 +1,10 @@
+//const,let,var
+
+let age = 23;
+age = 15;
+console.log(age);
+
+
+let ageTwo = 27;
+ageTwo = 45;
+console.log(ageTwo);
---
+++ variables/sampleAdd.js
@@ -0,0 +1,7 @@
+
+function add(a,b){
+    return a+b
+   
+}
+result = add(5,10)
+console.log(result);
