f3e095b726edd1b4aa4c5e970963137b7b898ab6
PraveenKola11 <107261366+PraveenKola11@users.noreply.github.com>
2023-08-17 22:07:56 +0530

    Array methods f commit

---
+++ practicePGMS/arrayOne.js
@@ -0,0 +1,15 @@
+//arrays are one of the data structures in JS. to store more than one value we go to data structures
+const nameArray =['saurav','sumanth','srikanth',35,85.46,true,false];
+console.log(nameArray);
+console.log(nameArray.length)
+console.log(nameArray[5]);
+
+nameArray.push('rakesh');
+console.log(nameArray);
+//pop will delete the last element of the arrayf
+nameArray.pop();
+console.log(nameArray);
+
+//given task====================
+//write a program to add the element at the beginig of the array
+//write a program on every method of the array
