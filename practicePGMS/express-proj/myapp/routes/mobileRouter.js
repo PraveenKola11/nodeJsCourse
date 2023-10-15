0c35ef2c12dce487c5d8c7bc63a2dbf986d38d7e
PraveenKola11 <107261366+PraveenKola11@users.noreply.github.com>
2023-10-15 17:56:35 +0530

    commit

--- practicePGMS/express-proj/myapp/routes/mobileRouter.js
+++ practicePGMS/express-proj/myapp/routes/mobileRouter.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/mobileDetails', function(req, res, next) {
-  res.render('mobileDetails', { mobileData: {name:'samsung s2', model:2023,processor:req.query.processor} });
+  const privateKey ='!@#$*545';
+  if(req.cookies.loggedIn){
+    jwt.verify(req.cookies.loggedIn.token, privateKey,{algorithm: 'RS256'} ,function(err, decoded) {
+      console.log(decoded.foo) // bar
+      res.render('mobileDetails', { mobileData: {name:'samsung s2', model:2023,processor:req.query.processor} });
+    });
+    
+  }else{
+    res.status(401).send('your session time out please login again');
+  }
+
 });
 router.get('/mobileDetails/:name', function(req, res, next) {
     res.render('mobileDetails', { mobileData: {name:'samsung s2', model:2023,name:req.params.name} });
